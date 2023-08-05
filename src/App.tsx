27ca59eab5b254b15e0e028cf856a2c2be8b0177
Jack Arthur Harrhy <me@jackharrhy.com>
2023-08-05 14:00:26 -0230

    the boy, no ui, rats move to him, he spits left, things die

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,9 @@
-import { GameCanvas, setPlaying } from "./GameCanvas";
+import { onMount } from "solid-js";
+import { initGame } from "./game/main";
 
 export const App = () => {
-  let canvas: HTMLCanvasElement | undefined;
-
-  return (
-    <>
-      <GameCanvas canvas={canvas} />
-      <div class="h-full bg-gray-900/50 p-8">
-        <header class="flex flex-col gap-4 m-auto w-64 bg-gray-100 p-4">
-          <p>gumpjam entry</p>
-          <button onClick={() => setPlaying(true)} class="bg-gray-300">
-            start
-          </button>
-        </header>
-      </div>
-    </>
-  );
+  onMount(async () => {
+    initGame();
+  });
+  return null;
 };
--- src/game/main.tsx
+++ src/game/main.tsx
@@ -1,26 +1,42 @@
+/* @refresh reload */
 import kaboom from "kaboom";
-import { playing, setPlaying } from "../GameCanvas";
 import crtFragShader from "./shaders/crt.frag?raw";
 import invertOverTimeFragShader from "./shaders/invert-over-time.frag?raw";
 
-const SPEED = 320;
+const SPEED = 160;
+const ENEMY_SPEED = SPEED * 0.5;
 
-export const initGame = (canvas: HTMLCanvasElement) => {
+export const initGame = () => {
   const {
     loadSprite: kaboomLoadSprite,
     loadShader,
     add,
+    loop,
     sprite,
     pos,
     rotate,
     anchor,
     center,
+    area,
+    destroy,
+    addKaboom,
     onUpdate,
+    onKeyDown,
+    onKeyRelease,
+    camPos,
+    make,
     usePostEffect,
     shader,
     time,
+    timer,
+    move,
+    circle,
+    color,
+    vec2,
   } = kaboom({
-    canvas,
+    width: 800,
+    height: 400,
+    letterbox: true,
     background: [255, 255, 255],
   });
 
@@ -30,63 +46,113 @@ export const initGame = (canvas: HTMLCanvasElement) => {
   const loadSprite = (name: string, path: string) =>
     kaboomLoadSprite(name, `${import.meta.env.BASE_URL}${path}`);
 
+  loadSprite("boy", "sprites/the-boy.png");
   loadSprite("rat", "sprites/rat.png");
 
+  /*
   usePostEffect("crt", () => ({
     u_flatness: 3,
   }));
+  */
 
-  const rat = add([
-    sprite("rat", {
-      width: 128,
+  const boy = add([
+    "boy",
+    sprite("boy", {
+      width: 32,
     }),
     pos(center()),
-    rotate(0),
+    rotate(360),
     anchor("center"),
+    area(),
+    /*
     shader("invert_over_time", () => ({
       u_time: time(),
     })),
+    */
+    timer(),
   ]);
 
-  const keys = {
-    ArrowLeft: () => {
-      rat.move(-SPEED, 0);
-    },
-    ArrowRight: () => {
-      rat.move(SPEED, 0);
-    },
-    ArrowUp: () => {
-      rat.move(0, -SPEED);
-    },
-    ArrowDown: () => {
-      rat.move(0, SPEED);
-    },
-    Escape: () => {
-      setPlaying(false);
-    },
-  } as const;
-
-  const isKeyPressed: Record<string, boolean> = Object.keys(keys).reduce(
-    (acc, key) => ({ ...acc, [key]: false }),
-    {}
-  );
-
-  onUpdate(() => {
-    Object.keys(keys).forEach((key) => {
-      if (!isKeyPressed[key]) return;
-      keys[key as keyof typeof keys]();
-    });
+  onKeyDown("left", () => {
+    boy.move(-SPEED, 0);
   });
 
-  canvas.onkeydown = (e) => {
-    if (!playing()) return;
-    if (isKeyPressed[e.key]) return;
-    isKeyPressed[e.key] = true;
+  onKeyDown("right", () => {
+    boy.move(SPEED, 0);
+  });
+
+  onKeyDown("up", () => {
+    boy.move(0, -SPEED);
+  });
+
+  onKeyDown("down", () => {
+    boy.move(0, SPEED);
+  });
+
+  boy.onUpdate(() => {
+    camPos(boy.worldPos());
+  });
+
+  boy.onCollide("rat", () => {
+    destroy(boy);
+    addKaboom(boy.pos, { scale: 0.5 });
+  });
+
+  const deg2rad = (deg: number) => deg * (Math.PI / 180);
+  const vecFromAngle = (angle: number, scale: number = 1) => {
+    const rad = deg2rad(angle);
+    return vec2(Math.cos(rad), Math.sin(rad)).scale(scale);
   };
 
-  canvas.onkeyup = (e) => {
-    if (!playing()) return;
-    if (!isKeyPressed[e.key]) return;
-    isKeyPressed[e.key] = false;
+  boy.loop(0.5, () => {
+    const vec = vecFromAngle(boy.angle, 8);
+
+    add([
+      "pew",
+      circle(3),
+      color(0, 0, 100),
+      pos(boy.pos.add(vec)),
+      move(vec, SPEED * 1.2),
+      area(),
+      anchor("center"),
+    ]);
+  });
+
+  const MIN_SPAWN_AWAY = 32 * 6;
+  const MAX_SPAWN_AWAY = MIN_SPAWN_AWAY * 2;
+  const range = MAX_SPAWN_AWAY - MIN_SPAWN_AWAY;
+
+  const addRat = () => {
+    const distance = MIN_SPAWN_AWAY + range * Math.random();
+    const angle = Math.random() * 360;
+    const vec = boy.pos.add(vecFromAngle(angle, distance));
+    const width = 32;
+    const rat = add([
+      "rat",
+      sprite("rat", {
+        width,
+      }),
+      pos(vec),
+      area(),
+      rotate(0),
+      anchor("center"),
+    ]);
+
+    rat.onUpdate(() => {
+      if (!boy.exists()) return;
+      const dir = boy.pos.sub(rat.pos).unit();
+      rat.move(dir.scale(ENEMY_SPEED));
+    });
+
+    rat.onCollide("pew", (pew) => {
+      destroy(pew);
+      destroy(rat);
+      addKaboom(rat.pos, { scale: 0.25 });
+    });
   };
+
+  loop(2, () => {
+    if (boy.exists()) {
+      addRat();
+    }
+  });
 };
